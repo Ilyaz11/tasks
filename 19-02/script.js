a0fe5a6b0de0e72a36c70e9a74d186b5482122b0
ilyaz11 <kurmanovilyaz.0311@gmail.com>
2022-02-24 13:28:27 +0600

    feat: added homework.js

---
+++ 19-02/script.js
@@ -0,0 +1,16 @@
+/*****1*****/
+
+let name1 = 'ilyaz';
+name2 = 'Kurmanov';
+name2 = name1;
+alert(name2);
+
+/*****2*****/
+
+let name = 'Ильяз';
+alert('Привет, ' + name);
+
+/*****3*****/
+
+let age = 18;
+alert('Мне ' + age + ' лет!');
