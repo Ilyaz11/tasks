93807d81762255810e024826dc7cb750c68c2686
ilyaz11 <kurmanovilyaz.0311@gmail.com>
2022-02-24 15:09:25 +0600

    feat: added homework

---
+++ 22-02/script.js
@@ -0,0 +1,18 @@
+const camelCase = 1;
+const snake_case = 2;
+const PaskalCase = 3;
+alert(camelCase);
+alert(snake_case);
+alert(PaskalCase);
+
+const str1 = 'Салам, ';
+const str2 = 'Бишкек!';
+const result = str1 + str2;
+alert(result);
+
+const a = 17;
+const b = 10;
+const c = a - b;
+const d = 7;
+const result1 = c +d;
+alert(result1);
