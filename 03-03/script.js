57cb55e3e9150a258148f6fcea10c36a07752816
ilyaz11 <kurmanovilyaz.0311@gmail.com>
2022-03-05 10:05:10 +0600

    feat: added homework03

---
+++ 03-03/script.js
@@ -0,0 +1,14 @@
+let arr = ['a', 'b', 'c', 'd'];
+console.log(arr[0] + arr[1], arr[2] + arr[3]);
+
+
+let num = [10, 15, 9, 10];
+let result = [num[0] * num[1] + num[2] * num[3]];
+console.log(result);
+
+
+let obj = ['a', 'b', 'c'];
+obj.push(1, 2, 3);
+console.log(obj);
+
+
