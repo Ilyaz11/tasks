933da912696f36b8a564091995e4af57956e9110
ilyaz11 <kurmanovilyaz.0311@gmail.com>
2022-03-12 15:19:08 +0600

    feat: added homework10

--- 05-03/script.js
+++ 05-03/script.js
@@ -1,32 +1,60 @@
-const arr = [1,2,3,11,4,7,8,10,12,18,9,1,5,17];
+/****** 1 ******/
+/*const arr = [1,2,3,11,4,7,8,10,12,18,9,1,5,17];
 let i;
 for (i = 0; i < arr.length; i++){
     if(arr[i] > 0 && arr[i] < 10){
         console.log(arr[i]);
     }
-}
-
-
+}*/
 
 /***** 2 *****/
-const num = [1,2,5,6,88,5];
+/*const num = [1,2,5,6,88,5];
 let sum = 0;
 for(let i = 0; i < num.length; i++){
     sum += num[i];
 }
-console.log(sum);
+console.log(sum);*/
 
 /****** 4 ******/
-const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+/*const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 for(i = 2; i <= 20; i++) {
     if (i % 2 === 0) {
         console.log(i);
     }
-}
+}*/
 
 /****** 5 ******/
+/*
 const arr1 = [];
 for(i = 0; i < 10; i++) {
     arr1[i] = 'x';
 }
-console.log(arr1);
+console.log(arr1);*/
+
+
+
+/****** 3 *******/
+const num2 = [2,4,5,3,6,3,2,1,4,10];
+let sum1 = 0;
+for(let i = 0; i < num2.length; i++) {
+    sum1 += num2[i] * num2[i];
+}
+console.log(sum1);
+
+
+/*const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for(let i = 2; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
