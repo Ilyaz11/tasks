72ba19ebc6d6623be23962c19e9d299dc9320e40
ilyaz11 <kurmanovilyaz.0311@gmail.com>
2022-03-15 14:33:19 +0600

    feat: added homework12

---
+++ 12-03/script.js
@@ -0,0 +1,23 @@
+/********** 1 **********/
+let hello = () => 'Привет, Javascript';
+console.log(hello());
+
+
+/********** 2 **********/
+let numCube = (x) => x * x * x;
+console.log(numCube(3));
+
+
+/********** 3 **********/
+let name = 'Василий';
+
+let getName = () => (name === undefined || name !== 'Василий' ) ? `Привет, гость` : `Привет, ${name}`;
+console.log(getName());
+
+
+/********** 4 **********/
+let array = [1, 2, 3, 4, 1, 1, 1, 4, 2, 2, 2, 2, 2, 2];
+
+let result = array.map(value => value + value);
+
+console.log(result);
